8d40a2ba20713a7311595272791761f77c23dc9f
hafid24 <menasria.hafidh.1>
2025-09-07 23:21:23 +0200

    [QA] new dockerfile

--- app/api/categories/route.ts
+++ app/api/categories/route.ts
@@ -1,5 +1,5 @@
-export const dynamic = "force-static";
-export const revalidate = 60;
+export const dynamic = "force-dynamic";
+
 import { NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import { Category } from "@/outils/types";
