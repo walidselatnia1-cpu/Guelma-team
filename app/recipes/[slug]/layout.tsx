4b86b875c8f6aa3f336cc996bd5a59d8b8b6c4c5
hafid24 <menasria.hafidh.1>
2025-09-17 15:59:09 +0200

    [FIX] [WIP] recipe page layout

--- app/recipes/[slug]/layout.tsx
+++ app/recipes/[slug]/layout.tsx
@@ -25,18 +25,18 @@ export default async function Layout({
   return (
     <div className="min-h-screen bg-gray-50">
       <main>
-        <div className="bg-stone-100 grid grid-cols-1 lg:grid-cols-12 xl:grid-cols-16 w-full px-4 pt-2  mx-auto">
+        <div className="bg-stone-100 grid grid-cols-1  xl:grid-cols-16 w-full px-4 pt-2  mx-auto">
           {/* Main Content */}
-          <div className="lg:col-span-2 xl:col-span-3"></div>
+          <div className=" xl:col-span-2 "></div>
 
           {/* Sidebar */}
-          <div className="lg:col-span-8 xl:col-span-10">
+          <div className=" xl:col-span-12">
             <div className="sticky">
               <RecipeHero recipe={recipe} />
             </div>
           </div>
 
-          <div className="lg:col-span-2 xl:col-span-3"></div>
+          <div className=" xl:col-span-2"></div>
         </div>
 
         {children}
